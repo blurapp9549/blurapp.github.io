78c3ca5de140fb46173474913ccb3baeaa5c405e
Praneeth V <praneeth.dev24@gmail.com>
2021-07-25 07:10:31 +0530

    typing js addition

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
 burgerIcon.addEventListener('click',() => {
     navbarMenu.classList.toggle('is-active');
 })
+
+  
+  var typed = new Typed('#typed', {
+    strings: ["Tired of post editing videos for hiding sensitive data", "Tired of closing tabs when you are screen sharing","Tired of Shoulder Surfing"],
+    typeSpeed: 50
+  });
